c064e83e07a12a2bf2896c6eb47af23be2563963
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 01:25:36 +0600

    feat: :bricks: protfolio page
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="hero-bg min-h-screen">
+        <div class="container mx-auto">
+            <Header />
+            <Hero />
+        </div>
+    </main>
+</template>
+<style scoped>
+.hero-bg {
+    background: linear-gradient(
+            45deg,
+            rgba(0, 0, 0, 0.366),
+            rgba(0, 0, 0, 0.547)
+        ),
+        url('~/assets/images/6690897_2352.jpg') no-repeat center/cover;
+    background-attachment: fixed;
+    width: 100%;
+}
+</style>
