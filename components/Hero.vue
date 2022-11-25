642e333c19f2721759bb119a1a21552d266aabdc
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 01:23:48 +0600

    feat: :construction: add hero section
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex py-10 mt-24 max-w-6xl mx-auto">
+        <div class="w-[50%]">
+            <button>Welcome to my Portfolio</button>
+            <h1 class="text-6xl my-8">Hi I'M Faysal</h1>
+            <p class="w-[70%]">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem
+                maxime consectetur ipsum dolorum veniam saepe quia vero numquam
+                reprehenderit facere.
+            </p>
+            <button class="mt-5">Hire Me</button>
+        </div>
+        <div class="w-[50%]">
+            <NuxtImg src="/images/hero-image.svg" width="100%" />
+        </div>
+    </div>
+</template>
