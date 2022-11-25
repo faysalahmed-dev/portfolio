b2679e4a2ed70cd65917844ccd5f5a5e726a964b
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 01:22:42 +0600

    feat: :building_construction: add nuxtpage comp and font
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtPage></NuxtPage>
+    <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap"
+            rel="stylesheet"
+        />
+        <!-- @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap'); -->
+    </Head>
 </template>
