f6ce3c0870539cd06150fa9d5ab3b7203a31cfef
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 02:25:16 +0600

    feat: :adhesive_bandage: fix hero bg path
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             rgba(0, 0, 0, 0.366),
             rgba(0, 0, 0, 0.547)
         ),
-        url('~/assets/images/6690897_2352.jpg') no-repeat center/cover;
+        url('~/assets/images/hero-bg.jpg') no-repeat center/cover;
     background-attachment: fixed;
     width: 100%;
 }
