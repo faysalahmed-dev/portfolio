8c0c5ac2a5c8e66ec82ed499109cc3b10e82cbd6
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 01:23:18 +0600

    feat: :sparkles: header and navbar
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="flex justify-between py-10">
+        <h3 class="text-xl font-bold">Faysal Ahmed</h3>
+        <div>
+            <ul class="flex justify-between space-x-5 font-bold">
+                <li><a href="">About Me</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Service</a></li>
+                <!-- <li><a href="">Experiences</a></li> -->
+                <li><a href="">Recent Work</a></li>
+                <!-- <li><a href="">Client Review</a></li> -->
+                <!-- <li><a href="">Recent Post</a></li> -->
+                <li><a href="">Get In Touch</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
