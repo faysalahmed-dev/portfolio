757c66db2c289676bb7b3e816a2bd4647f3a57f8
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-26 22:38:28 +0600

    feat: :lipstick: give body a background color
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
     <NuxtPage></NuxtPage>
     <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap"
             rel="stylesheet"
@@ -10,3 +10,9 @@
         <!-- @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap'); -->
     </Head>
 </template>
+<style>
+body {
+    background: rgb(47, 7, 67);
+    background: linear-gradient(to right, rgb(65, 41, 90), rgb(47, 7, 67));
+}
+</style>
