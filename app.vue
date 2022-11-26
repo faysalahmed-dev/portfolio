ec29e370999bb45534fe2d404ff47f84cffe14c0
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-27 01:28:35 +0600

    fix: :ambulance: import 300, 700, 800 gogole font
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -2,9 +2,9 @@
     <NuxtPage></NuxtPage>
     <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;600;700;800&display=swap"
             rel="stylesheet"
         />
         <!-- @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap'); -->
@@ -14,5 +14,6 @@
 body {
     background: rgb(47, 7, 67);
     background: linear-gradient(to right, rgb(65, 41, 90), rgb(47, 7, 67));
+    padding-bottom: 100px;
 }
 </style>
