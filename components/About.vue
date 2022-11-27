1b18d1bbb56a21f0b2b601b32777077e48a8802f
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-28 01:34:34 +0600

    feat: :lipstick: more rounded and change bg color
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- components/About.vue
+++ components/About.vue
@@ -1,25 +1,70 @@
 <template>
     <section
-        class="w-[1100px] mx-auto rounded-3xl py-20 px-16 about-section -mt-20"
+        class="max-w-[1100px] mx-auto rounded-[50px] py-10 px-16 about-section -mt-20"
     >
         <SectionHeading>About Me</SectionHeading>
-        <div class="flex">
-            <div class="w-2/4 flex justify-center items-center">
+        <div class="flex py-6">
+            <div class="flex-1 flex justify-center items-center">
                 <NuxtImg src="/images/faysalahmed.png" width="350" />
             </div>
-            <div class="w-2/4 p-4">
+            <div class="flex-1 p-4">
                 <div class="flex justify-between">
-                    <div class="p-4 rounded-md bg-purple-500 text-center">
+                    <div
+                        class="py-3 w-36 rounded-lg about-small-box text-center"
+                    >
+                        <svg
+                            class="inline-block mb-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            style="fill: currentColor; transform: ; msfilter: "
+                        >
+                            <path
+                                d="M16.999 23V7c0-1.103-.897-2-2-2h-8c-1.103 0-2 .897-2 2v16l6-3.601 6 3.601z"
+                            ></path>
+                            <path
+                                d="M15.585 3h1.414c1.103 0 2 .897 2 2v10.443l2 2.489V3c0-1.103-.897-2-2-2h-8c-1.103 0-2 .897-2 2h6.586z"
+                            ></path>
+                        </svg>
                         <h3 class="font-bold">Experience</h3>
                         <p>3+ years</p>
                     </div>
-                    <div class="p-4 rounded-md bg-purple-500 text-center">
-                        <h3 class="font-bold">Experience</h3>
-                        <p>3+ years</p>
+                    <div
+                        class="py-3 w-36 rounded-lg about-small-box text-center"
+                    >
+                        <svg
+                            class="inline-block mb-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            style="fill: currentColor; transform: ; msfilter: "
+                        >
+                            <path
+                                d="M20 3H7c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h6c1.103 0 2-.897 2-2h8c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM6 9h4l-.003 9H4V9h2zm6 8V9c0-1.103-.897-2-2-2H7V5h11v12h-6z"
+                            ></path>
+                        </svg>
+                        <h3 class="font-bold">Projects</h3>
+                        <p>10 +</p>
                     </div>
-                    <div class="p-4 rounded-md bg-purple-500 text-center">
-                        <h3 class="font-bold">Experience</h3>
-                        <p>3+ years</p>
+                    <div
+                        class="py-3 w-36 rounded-lg about-small-box text-center"
+                    >
+                        <svg
+                            class="inline-block mb-2"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            style="fill: currentColor; transform: ; msfilter: "
+                        >
+                            <path
+                                d="M4.5 8.552c0 1.995 1.505 3.5 3.5 3.5s3.5-1.505 3.5-3.5-1.505-3.5-3.5-3.5-3.5 1.505-3.5 3.5zM19 8h-2v3h-3v2h3v3h2v-3h3v-2h-3zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2z"
+                            ></path>
+                        </svg>
+                        <h3 class="font-bold">Clients</h3>
+                        <p>5 +</p>
                     </div>
                 </div>
                 <p class="my-10">
@@ -29,10 +74,10 @@
                     that work across all platforms & devices.
                 </p>
                 <div class="space-x-6">
-                    <button class="py-2 px-8 rounded-md bg-slate-700">
+                    <button class="py-2 px-8 rounded-lg bg-primary font-bold">
                         Let's Talk
                     </button>
-                    <button class="py-2 px-8 rounded-md bg-slate-700">
+                    <button class="py-2 px-8 rounded-lg bg-primary font-bold">
                         Download CV
                     </button>
                 </div>
@@ -42,11 +87,19 @@
 </template>
 <style scoped>
 .about-section {
-    background: linear-gradient(
-        to right,
-        rgb(55, 65, 81),
-        rgb(17, 24, 39),
-        rgb(0, 0, 0)
+    background: rgba(7, 3, 53, 1);
+    background-image: radial-gradient(
+        circle 610px at 5.2% 51.6%,
+        rgba(5, 8, 114, 1) 0%,
+        rgba(7, 3, 53, 1) 97.5%
     );
+}
+.about-small-box {
+    background-image: linear-gradient(
+        179.4deg,
+        rgba(12, 20, 69, 1) -16.9%,
+        rgba(71, 30, 84, 1) 119.9%
+    );
+    @apply shadow-md;
 }
 </style>
