54fbc0f156ff9a6fba4d0788e03521395b1ac7c4
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-27 19:33:50 +0600

    chore: :wrench: inject link to head and make util global comp
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    typescript: { shim: false },
+    typescript: { shim: true },
     modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
+    app: {
+        head: {
+            link: [
+                // <link rel="stylesheet" href="https://myawesome-lib.css">
+                { rel: 'stylesheet', href: 'https://awesome-lib.css' },
+                { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+                {
+                    rel: 'preconnect',
+                    href: 'https://fonts.gstatic.com',
+                    crossorigin: '',
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;600;700;800&display=swap',
+                    crossorigin: '',
+                },
+            ],
+        },
+    },
+    components: {
+        dirs: [{ path: '~/components/utils', global: true }, '~/components'],
+    },
+    ssr: false,
 });
