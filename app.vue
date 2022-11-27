2dc7159c714c0b587e3f4008331b4193645011db
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-11-27 19:34:56 +0600

    refactor: :recycle: rm link tag and add body style to app comp
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- app.vue
+++ app.vue
@@ -1,19 +1,15 @@
 <template>
     <NuxtPage></NuxtPage>
-    <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@300;400;600;700;800&display=swap"
-            rel="stylesheet"
-        />
-        <!-- @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@400;600&display=swap'); -->
-    </Head>
 </template>
 <style>
+:root {
+    --yello-light: #feef51;
+    --acen-light: #60f7ea;
+}
 body {
     background: rgb(47, 7, 67);
     background: linear-gradient(to right, rgb(65, 41, 90), rgb(47, 7, 67));
     padding-bottom: 100px;
+    @apply text-gray-300;
 }
 </style>
