2242e72510575d56fdc25111934ca210b97e11e2
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-12-02 23:09:56 +0600

    ci: :wrench: make container class centered and add padding
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        'pages/*.{html,vue}',
-        'components/*.{html,vue}',
+        './pages/*.{html,vue}',
+        './components/**/*.{html,vue}',
         './*.{vue,js,html}',
     ],
     theme: {
         extend: {},
         fontFamily: {
             sans: ["'Noto Sans Mono'", 'monospace'],
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
         },
     },
     plugins: [],
