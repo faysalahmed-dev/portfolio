01bcf447d3e2dddb59beca392b5146947218147e
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-12-02 23:14:21 +0600

    feat: :ambulance: wrap about to container cls
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <main class="hero-bg min-h-screen rounded-b-[50px]">
-        <div class="container mx-auto">
+    <!-- min-h-[95vh] md:min-h-screen min-h-[95vh]-->
+    <main class="hero-bg rounded-b-[50px] pb-48 lg:min-h-screen lg:pb-0">
+        <div class="container">
             <Header />
             <Hero />
         </div>
     </main>
-    <About />
+    <section class="container"><About /></section>
+
     <Skills />
     <Services />
     <Projects />
@@ -16,7 +18,9 @@
 <style scoped>
 .hero-bg {
     background: linear-gradient(207deg, #311358, rgba(0, 0, 0, 0.13)),
-        url(/_nuxt/assets/images/hero-bg.jpg) no-repeat center/cover;
+        url(/_nuxt/assets/images/hero-bg.jpg) no-repeat;
     width: 100%;
+    background-size: cover;
+    background-position: 0%;
 }
 </style>
